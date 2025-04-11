482b1f4d6a377ac9d5975f2fcf9193777cf56dc1
gorimaaa <juba.belhocine@outlook.fr>
2025-04-11 19:29:17 +0200

    Refactor Comment entity to include JSON annotations for bidirectional relationships with Post and User

--- backend/src/main/java/com/univrouen/socialmedia/Entity/Comment.java
+++ backend/src/main/java/com/univrouen/socialmedia/Entity/Comment.java
@@ -1,5 +1,7 @@
 package com.univrouen.socialmedia.Entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,15 +17,24 @@ import java.util.Date;
 @Table(name="Comments")
 public class Comment {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer commentId;
 
     private String content;
 
+    @JsonBackReference
     @ManyToOne
     @JoinColumn(
             name="post_id"
     )
     private Post post;
+
+    @JsonManagedReference
+    @ManyToOne
+    @JoinColumn(
+            name="author_id"
+    )
+    private User user;
 
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
