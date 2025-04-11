e727c815d2057aae3d777730755d0c0a2456524f
gorimaaa <juba.belhocine@outlook.fr>
2025-04-11 19:28:33 +0200

    Implement PostController and StdPostService for post creation, commenting, and voting on polls

--- backend/src/main/java/com/univrouen/socialmedia/Entity/Post.java
+++ backend/src/main/java/com/univrouen/socialmedia/Entity/Post.java
@@ -1,5 +1,7 @@
 package com.univrouen.socialmedia.Entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,20 +18,30 @@ import java.util.List;
 @Table(name="Posts")
 public class Post {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer postId;
 
     private String content;
 
+    @JsonManagedReference
     @ManyToOne
     @JoinColumn(
             name="author_id"
     )
     private User user;
 
+    @JsonManagedReference
     @OneToMany(
             mappedBy = "post"
     )
     private List<Comment> comments;
+
+    @JsonManagedReference
+    @OneToOne
+    @JoinColumn(
+            name = "poll_id"
+    )
+    private Poll poll;
 
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
