84cc4298dd558b6d6d708db0037b1a8db8dc523f
gorimaaa <juba.belhocine@outlook.fr>
2025-04-11 19:29:13 +0200

    Add PollOptionRepository and PollRepository interfaces; update UserRepository to use String as ID type

--- backend/src/main/java/com/univrouen/socialmedia/Repository/UserRepository.java
+++ backend/src/main/java/com/univrouen/socialmedia/Repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.univrouen.socialmedia.Repository;
 import com.univrouen.socialmedia.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
