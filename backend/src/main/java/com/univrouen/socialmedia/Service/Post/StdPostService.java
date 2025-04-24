ca29e2bd7bb24087b99c9a8a3209a5444324a9f4
gorimaaa <juba.belhocine@outlook.fr>
2025-04-24 19:40:36 +0200

    Add quiz creation functionality to StdPostService

--- backend/src/main/java/com/univrouen/socialmedia/Service/Post/StdPostService.java
+++ backend/src/main/java/com/univrouen/socialmedia/Service/Post/StdPostService.java
@@ -1,13 +1,12 @@
 package com.univrouen.socialmedia.Service.Post;
 
-import com.univrouen.socialmedia.Dto.Poll.PollDto;
-import com.univrouen.socialmedia.Dto.Poll.PollOptionDto;
+import com.univrouen.socialmedia.Dto.Post.Poll.PollDto;
+import com.univrouen.socialmedia.Dto.Post.Poll.PollOptionDto;
 import com.univrouen.socialmedia.Dto.Post.CreatePostRequestDto;
 import com.univrouen.socialmedia.Dto.Post.CreatePostResponseDto;
-import com.univrouen.socialmedia.Entity.Poll;
-import com.univrouen.socialmedia.Entity.PollOption;
-import com.univrouen.socialmedia.Entity.Post;
-import com.univrouen.socialmedia.Entity.User;
+import com.univrouen.socialmedia.Dto.Post.Quiz.QuizOptionDto;
+import com.univrouen.socialmedia.Dto.Post.Quiz.QuizQuestionDto;
+import com.univrouen.socialmedia.Entity.*;
 import com.univrouen.socialmedia.Repository.*;
 import jakarta.transaction.Transactional;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,17 +24,26 @@ public class StdPostService implements PostService {
     private CommentRepository commentRepository;
     private PollRepository pollRepository;
     private PollOptionRepository pollOptionRepository;
+    private QuizRepository quizRepository;
+    private QuizQuestionRepository quizQuestionRepository;
+    private QuizQuestionOptionRepository quizQuestionOptionRepository;
 
     public StdPostService(PostRepository postRepository,
                           UserRepository userRepository,
                           CommentRepository commentRepository,
                           PollRepository pollRepository,
-                          PollOptionRepository pollOptionRepository) {
+                          PollOptionRepository pollOptionRepository,
+                          QuizRepository quizRepository,
+                          QuizQuestionRepository quizQuestionRepository,
+                          QuizQuestionOptionRepository quizQuestionOptionRepository) {
         this.postRepository = postRepository;
         this.userRepository = userRepository;
         this.commentRepository = commentRepository;
         this.pollRepository = pollRepository;
         this.pollOptionRepository = pollOptionRepository;
+        this.quizRepository = quizRepository;
+        this.quizQuestionRepository = quizQuestionRepository;
+        this.quizQuestionOptionRepository = quizQuestionOptionRepository;
     }
 
 
@@ -48,6 +56,10 @@ public class StdPostService implements PostService {
         Optional<User> user = userRepository.findById(userId);
         if(user.isPresent()){
             post.setUser(user.get());
+            if(requestDto.getPoll() != null && requestDto.getQuiz() != null){
+                responseDto.setAlert("Il est interdit de créer un quiz et un sondage en même temps!");
+                return responseDto;
+            }
 
             if(requestDto.getPoll() != null){
                 PollDto pollDto = requestDto.getPoll();
@@ -65,9 +77,35 @@ public class StdPostService implements PostService {
                     pollOption.setPoll(poll);
                     pollOptionRepository.save(pollOption);
                 }
-
+                responseDto.setInfo("Le poste avec un sondage a été crée avec succès!");
+                return responseDto;
             }
 
+            if(requestDto.getQuiz() != null){
+                List<QuizQuestionDto> quizQuestionDtos = requestDto.getQuiz();
+
+                Quiz quiz = new Quiz();
+                quizRepository.save(quiz);
+                post.setQuiz(quiz);
+                postRepository.save(post);
+
+                List<QuizQuestion> quizQuestions = new ArrayList<>();
+                for(QuizQuestionDto quizQuestionDto : quizQuestionDtos){
+                    QuizQuestion quizQuestion = new QuizQuestion();
+                    quizQuestion.setQuestion(quizQuestionDto.getQuestion());
+                    quizQuestion.setQuiz(quiz);
+                    quizQuestionRepository.save(quizQuestion);
+                    for(QuizOptionDto quizOptionDto : quizQuestionDto.getQuiz_options()){
+                        QuizQuestionOption quizQuestionOption = new QuizQuestionOption();
+                        quizQuestionOption.setOption(quizOptionDto.getOption());
+                        quizQuestionOption.setIs_correct(quizOptionDto.getIs_correct());
+                        quizQuestionOption.setQuizQuestion(quizQuestion);
+                        quizQuestionOptionRepository.save(quizQuestionOption);
+                    }
+                }
+                responseDto.setInfo("Le poste avec un quiz a été crée avec succès!");
+                return responseDto;
+            }
             responseDto.setInfo("Le poste a été crée avec succès!");
             return responseDto;
         }else{
